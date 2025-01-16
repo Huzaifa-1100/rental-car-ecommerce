014d99b3d3a3b58baf7367f4b0c4e2e9367800d5
Huzaifa-1100 <asadhussainshad@gmail.com>
2025-01-16 13:20:04 +0500

    update api version

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   token: process.env.SANITY_ACCESS_TOKEN,
   projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset : "production",
-  apiVersion: "2024-12-26" ,
+  apiVersion: "v2024-12-26" ,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
